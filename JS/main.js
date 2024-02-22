729f8e2f2e6e6080d2aff7efa5837eddf47a9e5e
Aleksandr Chertkov <sasha-4704@mail.ru>
2024-02-23 00:40:19 +0300

    calculator

---
+++ JS/main.js
@@ -0,0 +1,17 @@
+var display = document.getElementById('window-input');
+		var check = 0;
+		function GoToDisplay(value) {
+			document.getElementById('window-input').value += value;
+		}
+
+		function Clear() {
+			document.getElementById('window-input').value = '';
+		}
+		function Calculator() {
+			try {
+				document.getElementById('window-input').value = eval(
+					document.getElementById('window-input').value)
+			} catch (error) {
+				document.getElementById('window-input').value = 'ERROR'
+			}
+		}
